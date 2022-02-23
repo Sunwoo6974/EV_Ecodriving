4ab04e2147e29579b5979045203ee4ef62d42fda
Sunwoo Kim <mscom456@icloud.com>
2022-02-23 16:14:09 +0900

    Delete 04_Sim_code directory

--- 04_Sim_code/DP_3state/plotvalue.m
+++
@@ -1,8 +0,0 @@
-function plotvalue(X,Y,Z,table,stage,Sh_mid,Vh_mid,Sf_mid)
-
-slice(X,Y,Z,table,Sh_mid,Vh_mid,Sf_mid)
-ylabel('Host Vehicle location [m]')
-xlabel('Host Vehicle Speed [m/s]')
-zlabel('Frontal Vehicle location [m]')
-title([num2str(stage),'-th stage'])
-colorbar;
